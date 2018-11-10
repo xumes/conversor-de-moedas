d7cc9de9b5fec7ab960aeb6141dcb28c3841809a
Reginaldo Santos <rmsantos@br.ibm.com>
2018-11-10 21:21:15 -0200

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = props => {
+    return (
+        <div className='jumbotron'>
+            <h1 className='display-4'>Conversor de Moedas</h1>
+            <p className='lead'>Do Zero ao Redux</p>
+        </div>
+    )
+}
+
+export default Header
