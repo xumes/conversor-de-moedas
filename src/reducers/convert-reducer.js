ee3dacc38de74e3f8f36dc0138e785bc1b65e9fd
Reginaldo Santos <rmsantos@br.ibm.com>
2019-02-10 16:59:40 -0200

    criando o reducer convert

---
+++ src/reducers/convert-reducer.js
@@ -0,0 +1,13 @@
+import { CONVERTE_MOEDAS } from "./../actions";
+const INITIAL_STATE = [];
+
+export default function(state = INITIAL_STATE, action) {
+    if (action.type == CONVERTE_MOEDAS) {
+        const result = action.payload.data
+
+        return result
+    } else {
+        return state;
+    }
+  
+}
